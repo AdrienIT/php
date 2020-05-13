e0a94b6a7524d8daeef7b3d884a2b58137cf3cf7
AdrienIT <59971358+AdrienIT@users.noreply.github.com>
2020-05-14 01:38:14 +0200

    Delete connect.php

--- tp_note/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$db = new PDO("mysql:host=localhost; dbname=tp_note",'root','');
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	if (!$db) {
-		die("Bad Connection");
-	}
-?>
