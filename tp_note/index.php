3aeaa093cd43f4ed2d0983bb17c9cf0165390643
AdrienIT <59971358+AdrienIT@users.noreply.github.com>
2020-05-17 17:53:20 +0200

    Add files via upload

--- tp_note/index.php
+++ tp_note/index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
 	<meta charset="UTF-8">
 	<title>Choix du compte</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
+
 <body>
-	<a href="./user/index.php">→ Section Utilisateurs</a><br><br>
-	<a href="./admin/index.php">→ Section Administration</a><br><br>
-	<a href="./entreprise/index.php">→ Section Entreprise</a>
+
+	<div>
+		<a href="./user/index.php">→ Section Utilisateurs</a><br><br>
+		<a href="./admin/index.php">→ Section Administration</a><br><br>
+		<a href="./entreprise/index.php">→ Section Entreprise</a>
+	</div>
+
 </body>
+
 </html>
--- tp_note/offres/index.php
+++ tp_note/offres/index.php
@@ -1,1 +1,12 @@
-ptdr lol
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Merci de vous connecter !</p>
+    <a href="../index.php"> Retour à la connexion</a>
+</body>
+</html>
